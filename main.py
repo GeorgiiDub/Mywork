534886467d01193b90467096e9a3a4c4ab6b364f
64646 <dubinin6889@gmail.com>
2022-02-05 22:30:10 +0300

    Add file

---
+++ main.py
@@ -0,0 +1,101 @@
+from tkinter import *
+from tkinter import messagebox as mb
+from tkcalendar import DateEntry
+
+
+window = Tk()
+window.title("My Work")
+window.geometry("1200x600")
+
+var_date = StringVar()
+var_topic = StringVar()
+var_project = StringVar()
+var_duration = StringVar()
+var_contacts = StringVar()
+var_data_connect = StringVar()
+var_vcs = StringVar()
+var_meet_room = StringVar()
+var_content = StringVar()
+
+'''добавить поле время продумать как его лучше реализовать
+проработать поля, чтобы соответствовали значениям, переключатели, флажки, выбор из списка'''
+
+
+def write_meet(x, y, z):
+    y=str(y)
+    f = open(y+'_'+z + '.txt', 'w')
+    f.write(x)
+    f.close()
+
+def save1():
+    var_date = txt_date.get_date()
+    var_topic = txt_topic.get()
+    var_project = txt_project.get()
+    var_duration = txt_duration.get()
+    var_contacts = txt_duration.get()
+    var_data_connect = txt_data_connect.get()
+    var_vcs = txt_vcs.get()
+    var_meet_room = txt_meet_room.get()
+    var_content = txt_content.get()
+
+    result1 =(f'{lbl_date["text"]}:{var_date}\n{lbl_topic["text"]}:{var_topic}\n{lbl_project["text"]}:{var_project}\n\
+{lbl_duration["text"]}:{var_duration}\n{lbl_contacts["text"]}:{var_contacts}\n{lbl_data_connect["text"]}:{var_data_connect}\n\
+{lbl_vcs["text"]}:{var_vcs}\n{lbl_meet_room["text"]}:{var_meet_room}\n{lbl_content["text"]}:{var_content}\n')
+    write_meet(result1, var_date, var_topic)
+    mb.showinfo("записалось", result1)
+
+lbl_date = Label(window, text="1. Дата и время", relief=GROOVE)
+lbl_date.grid(column=0, row=1, ipadx=5, ipady=5, sticky=E, padx=3, pady=3)
+txt_date = DateEntry(window, width=12, textvariable=var_date, date_pattern='dd/mm/yyyy')
+txt_date.grid(column=1, row=1, ipadx=5, ipady=5, sticky=W, padx=3, pady=3)
+
+
+lbl_topic = Label(window, text="2. Тема совещания", relief=GROOVE)
+lbl_topic.grid(column=0, row=2, ipadx=5, ipady=5, sticky=E, padx=3, pady=3)
+txt_topic = Entry(window, width=30, textvariable=var_topic)
+txt_topic.grid(column=1, row=2, ipadx=5, ipady=5, sticky=W, padx=3, pady=3)
+
+lbl_project = Label(window, text="3. Проект", relief=GROOVE)
+lbl_project.grid(column=0, row=3, ipadx=5, ipady=5, sticky=E, padx=3, pady=3)
+txt_project = Entry(window, width=30, textvariable=var_project)
+txt_project.grid(column=1, row=3, ipadx=5, ipady=5, sticky=W, padx=3, pady=3)
+
+lbl_duration = Label(window, text="4. Продолжительность", relief=GROOVE)
+lbl_duration.grid(column=0, row=4, ipadx=5, ipady=5, sticky=E, padx=3, pady=3)
+txt_duration = Entry(window, width=30, textvariable=var_duration)
+txt_duration.grid(column=1, row=4, ipadx=5, ipady=5, sticky=W, padx=3, pady=3)
+
+lbl_contacts = Label(window, text="5. Контакты организатора", relief=GROOVE)
+lbl_contacts.grid(column=0, row=5, ipadx=5, ipady=5, sticky=E, padx=3, pady=3)
+txt_contacts = Entry(window, width=30, textvariable=var_contacts)
+txt_contacts.grid(column=1, row=5, ipadx=5, ipady=5, sticky=W, padx=3, pady=3)
+
+lbl_data_connect = Label(window, text="6. Данные на покдлючение входные", relief=GROOVE)
+lbl_data_connect.grid(column=0, row=6, ipadx=5, ipady=5, sticky=E, padx=3, pady=3)
+txt_data_connect = Entry(window, width=30, textvariable=var_data_connect)
+txt_data_connect.grid(column=1, row=6, ipadx=5, ipady=5, sticky=W, padx=3, pady=3)
+
+lbl_vcs = Label(window, text="7. Система ВКС", relief=GROOVE)
+lbl_vcs.grid(column=0, row=7, ipadx=5, ipady=5, sticky=E, padx=3, pady=3)
+txt_vcs = Entry(window, width=30, textvariable=var_vcs)
+txt_vcs.grid(column=1, row=7, ipadx=5, ipady=5, sticky=W, padx=3, pady=3)
+
+lbl_meet_room = Label(window, text="8. Переговорные комнаты", relief=GROOVE)
+lbl_meet_room.grid(column=0, row=8, ipadx=5, ipady=5, sticky=E, padx=3, pady=3)
+txt_meet_room = Entry(window, width=30, textvariable=var_meet_room)
+txt_meet_room.grid(column=1, row=8, ipadx=5, ipady=5, sticky=W, padx=3, pady=3)
+
+lbl_content = Label(window, text="9. Демонстрация материалов", relief=GROOVE)
+lbl_content.grid(column=0, row=9, ipadx=5, ipady=5, sticky=E, padx=3, pady=3)
+txt_content = Entry(window, width=30, textvariable=var_content)
+txt_content.grid(column=1, row=9, ipadx=5, ipady=5, sticky=W, padx=3, pady=3)
+
+
+
+btn_write = Button(window, text="Записать", bg="#abd9ff", command=save1)
+btn_write.grid(column=0, row=10, ipadx=5, ipady=5, sticky=E, padx=3, pady=3)
+
+
+
+
+window.mainloop()
